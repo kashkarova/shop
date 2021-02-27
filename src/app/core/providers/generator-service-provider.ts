a941c6da31c7a7bea8069170b684413441a11a34
Tetiana Kashkarova <kashkarovatany@gmail.com>
2021-02-27 03:56:14 +0200

    Implemented local-storage service

--- src/app/core/providers/generator-service-provider.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigOptionsService } from "../services/config-options.service";
-import { ConstantsService } from "../services/constants.service";
-import { GeneratorService } from "../services/generator.service";
-
-export const GeneratorServiceProvider = {
-  provide: GeneratorService,
-  useFactory: GeneratorFactory,
-  deps: [ConstantsService, ConfigOptionsService]
-}
--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+  useValue: new LocalStorageService()
 })
 export class LocalStorageService {
 
   constructor() { }
 
-  public setItem(){
-    
+  public setItem(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  public getItemByKey(key: string): string {
+    const value = localStorage.getItem(key);
+    if (!value) {
+      console.log('Error! There is no value with such key! ');
+      return null;
+    }
+
+    return value;
   }
 }
